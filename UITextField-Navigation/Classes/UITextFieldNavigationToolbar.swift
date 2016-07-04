4abfe64f064987a89a578c0ee14619d37bc972fe
Thanh Pham <minhthanh@me.com>
2016-07-04 14:38:23 +0800

    Update the next and previous icons

--- UITextField-Navigation/Classes/UITextFieldNavigationToolbar.swift
+++ UITextField-Navigation/Classes/UITextFieldNavigationToolbar.swift
@@ -31,8 +31,8 @@ public class UITextFieldNavigationToolbar: UIToolbar {
     }
 
     init() {
-        previousButton = UITextFieldNavigationToolbarButtonItem(title: "❬", style: .Plain, target: nil, action: nil)
-        nextButton = UITextFieldNavigationToolbarButtonItem(title: "❭", style: .Plain, target: nil, action: nil)
+        previousButton = UITextFieldNavigationToolbarButtonItem(title: " ❮ ", style: .Plain, target: nil, action: nil)
+        nextButton = UITextFieldNavigationToolbarButtonItem(title: " ❯ ", style: .Plain, target: nil, action: nil)
         doneButton = UITextFieldNavigationToolbarButtonItem(title: "Done", style: .Plain, target: nil, action: nil)
 
         super.init(frame: CGRectZero)
