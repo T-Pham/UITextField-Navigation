a6fcf1cf00c347ef5f1c7117bc5cc08b8b64f07e
Thanh Pham <minhthanh@me.com>
2018-07-16 10:14:10 +0800

    Example on connecting next text field from UITextField to UITextView

--- Example/UITextField-Navigation/ViewController.swift
+++ Example/UITextField-Navigation/ViewController.swift
@@ -11,34 +11,36 @@ import UITextField_Navigation
 
 class ViewController: UIViewController {
     @IBOutlet var textFieldIB1: UITextField!
-    @IBOutlet var textView: UITextView!
+    @IBOutlet var textViewIB2: UITextView!
     @IBOutlet var textFieldIB3: UITextField!
     var textFieldCode1: UITextField!
-    var textFieldCode2: UITextField!
+    var textViewCode2: UITextView!
     var textFieldCode3: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setUpBorder(for: textViewIB2)
+
         textFieldCode1 = createRightTextField("UITextField Code 1", leftField: textFieldIB1)
-        textFieldCode2 = createRightTextField("UITextField Code 2", leftField: textView)
+        textViewCode2 = createRightTextView("UITextView Code 2", leftField: textViewIB2)
         textFieldCode3 = createRightTextField("UITextField Code 3", leftField: textFieldIB3)
 
         textFieldIB3.nextNavigationField = textFieldCode1
-        textFieldCode1.nextNavigationField = textFieldCode2
-        textFieldCode2.nextNavigationField = textFieldCode3
+        textFieldCode1.nextNavigationField = textViewCode2
+        textViewCode2.nextNavigationField = textFieldCode3
 
-        textFieldCode2.delegate = self
-        textFieldCode2.navigationFieldToolbar?.barStyle = .default
-        textFieldCode2.navigationFieldToolbar?.backgroundColor = .red
-        textFieldCode2.navigationFieldToolbar?.previousButton.title = "Previous"
-        textFieldCode2.navigationFieldToolbar?.nextButton.title = "Next"
-        textFieldCode2.navigationFieldToolbar?.doneButton.title = "Dismiss"
+        textFieldCode3.delegate = self
+        textFieldCode3.navigationFieldToolbar?.barStyle = .default
+        textFieldCode3.navigationFieldToolbar?.backgroundColor = .red
+        textFieldCode3.navigationFieldToolbar?.previousButton.title = "Previous"
+        textFieldCode3.navigationFieldToolbar?.nextButton.title = "Next"
+        textFieldCode3.navigationFieldToolbar?.doneButton.title = "Dismiss"
 
         let customButton = UIBarButtonItem(title: "Custom", style: .plain, target: nil, action: nil)
         customButton.tintColor = .white
         let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-        textFieldCode2.navigationFieldToolbar?.items = [textFieldCode2.navigationFieldToolbar!.previousButton, textFieldCode2.navigationFieldToolbar!.nextButton, customButton, flexibleSpace, textFieldCode2.navigationFieldToolbar!.doneButton]
+        textFieldCode3.navigationFieldToolbar?.items = [textFieldCode3.navigationFieldToolbar!.previousButton, textFieldCode3.navigationFieldToolbar!.nextButton, customButton, flexibleSpace, textFieldCode3.navigationFieldToolbar!.doneButton]
     }
 
     func createRightTextField(_ placeholder: String, leftField: UIView) -> UITextField {
@@ -50,6 +52,23 @@ class ViewController: UIViewController {
         view.addSubview(textField)
         view.addConstraints([NSLayoutConstraint(item: textField, attribute: .width, relatedBy: .equal, toItem: leftField, attribute: .width, multiplier: 1, constant: 0), NSLayoutConstraint(item: textField, attribute: .centerY, relatedBy: .equal, toItem: leftField, attribute: .centerY, multiplier: 1, constant: 0), NSLayoutConstraint(item: textField, attribute: .left, relatedBy: .equal, toItem: leftField, attribute: .right, multiplier: 1, constant: 15)])
         return textField
+    }
+
+    func createRightTextView(_ text: String, leftField: UIView) -> UITextView {
+        let textView = UITextView()
+        setUpBorder(for: textView)
+        textView.font = UIFont.systemFont(ofSize: 14)
+        textView.text = text
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(textView)
+        view.addConstraints([NSLayoutConstraint(item: textView, attribute: .width, relatedBy: .equal, toItem: leftField, attribute: .width, multiplier: 1, constant: 0), NSLayoutConstraint(item: textView, attribute: .height, relatedBy: .equal, toItem: leftField, attribute: .height, multiplier: 1, constant: 0), NSLayoutConstraint(item: textView, attribute: .centerY, relatedBy: .equal, toItem: leftField, attribute: .centerY, multiplier: 1, constant: 0), NSLayoutConstraint(item: textView, attribute: .left, relatedBy: .equal, toItem: leftField, attribute: .right, multiplier: 1, constant: 15)])
+        return textView
+    }
+
+    func setUpBorder(for textView: UITextView) {
+        textView.layer.borderColor = UIColor.lightGray.cgColor
+        textView.layer.borderWidth = 1
+        textView.layer.cornerRadius = 4
     }
 }
 
