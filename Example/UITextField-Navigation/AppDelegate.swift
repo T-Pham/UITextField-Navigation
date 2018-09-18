0f562817f131e73ce16ff5f8fcb0e9cb5d2fe7b7
Thanh Pham <minhthanh@me.com>
2018-09-19 00:48:05 +0800

    Xcode 10 & Swift 4.2 support

--- Example/UITextField-Navigation/AppDelegate.swift
+++ Example/UITextField-Navigation/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 //        UITextField_Navigation.Config.previousButtonTitle = "Previous"
 //        UITextField_Navigation.Config.nextButtonTitle = "Next"
 //        UITextField_Navigation.Config.doneButtonTitle = "Done"
